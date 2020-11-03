3bc9d62a4939c64c75db5cb0e4d70b1c7a5e27fd
Jena Boehm <boehmjena@gmail.com>
2020-11-03 08:26:16 -0800

    Add data and get working in postman

---
+++ data/movies.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'Harry Potter and the Sorcerer\'s Stone',
+    year: 2001,
+    oscars: false,
+    genre: 'fantasy'
+  },
+  {
+    name: 'The Dark Knight',
+    year: 2008,
+    oscars: true,
+    genre: 'superhero'
+  },
+  {
+    name: 'Titanic',
+    year: 1997,
+    oscars: true,
+    genre: 'romance'
+  },
+  {
+    name: 'Dirty Dancing',
+    year: 1987,
+    oscars: true,
+    genre: 'romance'
+  }
+];
